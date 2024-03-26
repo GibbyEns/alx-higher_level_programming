4d9b9085587bf6f8db05170598be7dfae3442091
GibbyEns <gibbymerc@gmail.com>
2024-03-26 03:17:05 +0200

    if loops

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,7 @@
+
+#!/usr/bin/python3
+w(a, b):
+        return (a ** b)
+
+def pow(a, b):
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+ in range(ord('a'), ord('z') + 1):
+         print('{:c}'.format(i), end='')
+
+
